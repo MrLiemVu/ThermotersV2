f1e7a2ff16489559b9b365d0f92d565aadf71ffe
Liem Vu <50932746+MrLiemVu@users.noreply.github.com>
2024-07-09 16:31:07 -0700

    put docstrings in general_functions

--- functions/general_functions.py
+++ functions/general_functions.py
@@ -1,50 +1,97 @@
 
 # find positions of elements in the list
-def pozicija(testlist,cond):
+def pozicija(testlist, cond):
+    '''
+    This function returns a list of indices of elements
+    in `testlist` that satisfy the condition `cond`.
+    
+    Parameters:
+        testlist : The list to search.
+        cond : The condition to satisfy.
+    
+    Returns:
+        [i for i,x in enumerate(testlist) if cond(x)] : The list of indices of elements that satisfy the condition.
+    '''
     return [i for i,x in enumerate(testlist) if cond(x)]
 
 def polynom(c, x):
+    '''
+    Calculate a polynomial.
+    
+    Parameters:
+        c : The coefficients of the polynomial.
+        x : The variable of the polynomial.
+    
+    Returns:
+        npsum(out, axis=0) : The value of the polynomial.
+    '''
     from numpy import sum as npsum
+    
     out = [k*x**j for j,k in enumerate(c)]
     return npsum(out, axis=0)
 
 def is_number(x):
+    return isinstance(x, int) # Test this
     try:
         int(eval(x))
         return True
     except:
         return False
 
-def mode(l):
+def mode(l): # Why not use np mode?
+    
     from collections import Counter
+    
     return Counter(l).most_common(1)[0][0]
 
-
-# def autocorr(sett, dtrange):
-#     from numpy import zeros, corrcoef
-#     ret = zeros(len(dtrange))
-#     for k,i in enumerate(dtrange):
-#         if i==0:
-#             ret[k] = 1.
-#         else:
-#             ret[k] = corrcoef(sett[:len(sett)-i],sett[i:])[0,1]
-#     return ret
-
-
 def moving_average(a, n=3) :
+    '''
+    Calculate the moving average of a time series.
+    
+    Parameters:
+        a : The time series.
+        n : The number of points to average.
+    
+    Returns:
+        ret[n - 1:] / n : The moving average of the time series.
+    '''
     from numpy import cumsum
+    
     ret = cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:] / n
 
 def moving_sum(a, n=3) :
+    '''
+    Calculate the moving sum of a time series.
+    
+    Parameters:
+        a : The time series.
+        n : The number of points to sum.
+    
+    Returns:
+        ret[n - 1:] / n : The moving sum of the time series.
+    '''
     from numpy import cumsum
+    
     ret = cumsum(a, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
     return ret[n - 1:]
 
 def autocorr(sett, dtrange, nsplits = 1):
+    '''
+    Autocorrelation function.
+    
+    Parameters:
+        sett : The time series.
+        dtrange : The range of time lags.
+        nsplits : The number of splits.
+    
+    Returns:
+        out : The autocorrelation function.
+    '''
     from numpy import zeros, corrcoef, array, mean, std
+    
     if nsplits == 1:
         ret = zeros(len(dtrange))
         for k,i in enumerate(dtrange):
@@ -74,7 +121,8 @@ def OU(theta,mu,sigma,tmax,x0,dt):
     
     Parameters:
     
-    Returns
+    Returns:
+    
     '''
     from random import randn
     from numpy import empty
@@ -88,21 +136,52 @@ def OU(theta,mu,sigma,tmax,x0,dt):
     b  = sigma*dt**.5*w
     for t in range(maxindex-1):
         x[t+1] = a1*x[t] - a2 + b[t]
+    
     return x
 
 
 def order(testlist):
+    '''
+    Return the order of elements in a list.
+    
+    Parameters:
+        testlist : The list to order.
+    
+    Returns:
+        out : The order of the elements in the list.
+    '''
     import numpy as np
+    
     tmp = sorted([[i,el] for i,el in enumerate(testlist)], key=lambda xi: xi[1])
     return np.array([el[0] for el in tmp])
     
 def tally(mylist):
+    ''' 
+    Tally elements in a list
+    
+    Parameters:
+        mylist : The list to tally.
+    
+    Returns:
+        out : The tally of the elements in the list.
+    '''
     from collections import Counter
-    import numpy as np
+    
     return sorted(Counter(mylist).most_common(), key=lambda duple: duple[0])
 
 
 def multi_map(some_function, iterable, processes=1):
+    '''
+    Apply a function to an iterable using multiple processes.
+    
+    Parameters:
+        some_function : The function to apply to the iterable.
+        iterable : The iterable to apply the function to.
+        processes : The number of processes to use.
+    
+    Returns:
+        out : The output of the function applied to the iterable.
+    '''
     assert type(processes) == int
     if processes==1:
         out = map(some_function, iterable)
@@ -121,7 +200,9 @@ def multi_map(some_function, iterable, processes=1):
 from contextlib import contextmanager
 @contextmanager
 def suppress_stdout():
+    ''' Suppress the output of a block of code. '''
     import sys, os
+    
     with open(os.devnull, "w") as devnull:
         old_stdout = sys.stdout
         sys.stdout = devnull
@@ -137,6 +218,23 @@ extraColors = [ u'firebrick', u'darkolivegreen', u'indigo', u'indianred', u'dark
 
 
 def stochasticMaximize(fun,x0,steps = 10000, temp = 1., step = .1):
+    '''
+    Stochastic optimization of a function.
+    
+    Parameters:
+        fun : The function to optimize.
+        x0 : The initial guess.
+        steps : The number of steps to take.
+        temp : The temperature of the system.
+        step : The step size.
+    
+    Returns:
+        outPars : The optimized parameters.
+        fun(outPars) : The value of the function at the optimized parameters.
+    '''
+    import numpy as np
+    from MCMC import MCMC
+    
     global exponent, mcmc
     execfile('MCMCworker_RNApOnly_exclusions.py')
     from os.path import expanduser
@@ -147,9 +245,6 @@ def stochasticMaximize(fun,x0,steps = 10000, temp = 1., step = .1):
     outPars = np.loadtxt(mcmc.filename+".out", skiprows=steps//10//nPars*9//10)[:,1:-1].mean(axis=0)
     return outPars, fun(outPars)
 
-
-from collections import Mapping, Container
-from sys import getsizeof
  
 def deep_getsizeof(o, ids):
     """Find the memory footprint of a Python object
@@ -166,6 +261,9 @@ def deep_getsizeof(o, ids):
     :param ids:
     :return:
     """
+    from collections import Mapping, Container
+    from sys import getsizeof
+
     d = deep_getsizeof
     if id(o) in ids:
         return 0
